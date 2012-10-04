1c4c7708cd596f840822afcf38da7bb7e6de0f1e
James Tomasino <james@tomasino.org>
2012-10-03 22:31:15 -0400

    Using handlebars templates via ajax and json data to build structure

--- sjs/main.js
+++ sjs/main.js
@@ -1,4 +1,43 @@
 // Example of library includes
 //= require lib/modernizr
 //= require lib/jquery-1.8.2
+//= require lib/handlebars-1.0.rc.1
 //= require lib/l10n
+
+window.console||(window.console={log:function(){}});
+
+// Load Handlebars templates and json data
+var sectionTemplatePath = 'templates/section.handlebars';
+var itemTemplatePath = 'templates/item.handlebars';
+var dataPath = 'data/data.json';
+
+$.when (
+	$.ajax ( sectionTemplatePath ),
+	$.ajax ( itemTemplatePath ),
+	$.getJSON ( dataPath )
+).then( onDataLoad, onDataFail );
+
+function onDataLoad ( section, item, data ) {
+	var sectionHTML = section[0];
+	var itemHTML = item[0];
+	var jsonData = data[0];
+	var sectionTemplate  = Handlebars.compile(sectionHTML);
+	var itemTemplate     = Handlebars.compile(itemHTML);
+
+	// Process Data
+	$.each ( jsonData.section, function ( index, data) {
+		var html = $(sectionTemplate(data));
+		var items = html.find('.items');
+
+		$.each ( data.item, function ( index, data ) {
+			var itemHTML = $(itemTemplate(data));
+			items.append(itemHTML);
+		} );
+
+		$('#links').append(html);
+	} );
+}
+
+function onDataFail ( error ) {
+	$('#links').html('<h1 class="error">There was an error loading site data.</h1>')
+}
