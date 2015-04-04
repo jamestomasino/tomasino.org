32aa47ace9a6568f2bff68ad79fe13d83c5943ec
James Tomasino <james@tomasino.org>
2015-04-03 21:02:25 -0400

    added Delegate class back in

---
+++ js/lib/Delegate.js
@@ -0,0 +1,11 @@
+(function(){
+	"use strict";
+
+	var namespace = new NS ( 'lib' );
+	namespace.Delegate = function(fn, context) {
+		return function() {
+			fn.apply(context, arguments);
+		};
+	};
+
+})();
